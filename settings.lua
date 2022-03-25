e315c1398dcb934f0914f41613615fb376349455
Archie Halliwell <archie@halliwell.com.au>
2022-03-25 21:44:01 +1100

    Change to use new function

--- settings.lua
+++ settings.lua
@@ -1,18 +1,3 @@
-data:extend({
-    {
-        type = "int-setting",
-        name = "polygondwanaland-menu-track",
-        setting_type = "startup",
-        default_value = 0,
-        minimum_value = 0,
-        maximum_value = 10
-    }
-})
-data:extend({
-    {
-        type = "bool-setting",
-        name = "polygondwanaland-include-menu-track",
-        setting_type = "startup",
-        default_value = false
-    }
-})
+local musitorio = require("__libmusitorio__.functions.musitorio")
+
+musitorio_settings("polygondwanaland", 10)
